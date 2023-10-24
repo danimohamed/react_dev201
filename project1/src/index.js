03a44ccbaadd4a15e5b9a75e81e9bf06ffebdcaa
danimohamed <mohameddani993@gmail.com>
2023-10-24 10:31:47 +0100

    (File)

--- project1/src/index.js
+++ project1/src/index.js
@@ -1,6 +1,6 @@
 //les aspects avancés de javascript
 //1-creation d'un classe
-class Etudiant{
+/*class Etudiant{
     constructor(nom,age){
         this.nom=nom;
         this.age=age;
@@ -27,3 +27,43 @@ class stagiere extends Etudiant{
 }
 let stagiere1=new stagiere("mohamed",20,"dev 201");
 console.log(stagiere1.affichage2());
+*/
+
+
+/*
+class Personne{
+    constructor(nom,prenom,age){
+        this.nom=nom;
+        this.prenom=prenom;
+        this.age=age;
+    }
+    aff(){
+        return `${this.nom},${this.prenom} ,${this.age} ans`;
+    }
+}
+class Eomploye extends Personne{
+    constructor(nom,prenom,age,salaire){
+        super(nom,prenom,age);
+        this.salaire=salaire;
+    }
+    aff(){
+        return `${super.aff()} -salaire : ${this.salaire} `;
+    }
+}
+
+class Chef extends Eomploye{
+    constructor(nom,prenom,age,salaire,departement){
+        super(nom,prenom,age,salaire);
+        this.departement=departement;
+    }
+    aff(){
+        return `${super.aff()} -departement : ${this.departement} `;
+    }
+}
+
+let p1=new Personne("mohamed","ali",20);
+let e1=new Eomploye("mohamed","ali",20,2000);
+let c1=new Chef("mohamed","ali",20,2000,"informatique");
+console.log(p1.aff());
+console.log(e1.aff());
+console.log(c1.aff());*/
