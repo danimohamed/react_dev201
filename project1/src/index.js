2c11c051783f4edbd6328fdf55e76b9ae14a1b6f
danimohamed <mohameddani993@gmail.com>
2023-10-24 09:54:45 +0100

    aspects avancés de javascrip

--- project1/src/index.js
+++ project1/src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+//les aspects avancés de javascript
+//1-creation d'un classe
+class Etudiant{
+    constructor(nom,age){
+        this.nom=nom;
+        this.age=age;
+        
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    }
+    affichage(){
+        return `Etudiant,${this.nom},"age:",${this.age},"ans" `;
+    }
+}
+let etudiant1=new Etudiant("mohamed",20);
+let etudiant2=new Etudiant("ali",30);
+//console.log("Etudiant",etudiant1.nom,"age:",etudiant1.age,"ans");
+console.log(etudiant1.affichage());
+//2) heritage de classe
+class stagiere extends Etudiant{
+    constructor(nom,age,filiere){
+        super(nom,age);
+        this.filiere=filiere;
+    }
+    affichage2(){
+        return `${this.nom},"age:",${this.age},"ans","filiere:",${this.filiere} `;
+    }
+}
+let stagiere1=new stagiere("mohamed",20,"dev 201");
+console.log(stagiere1.affichage2());
