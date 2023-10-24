5b71a8d6ef38f1eb2cd04c9edd663465490ee84f
danimohamed <mohameddani993@gmail.com>
2023-10-24 13:21:07 +0100

    react

--- project1/src/index.js
+++ project1/src/index.js
@@ -67,3 +67,58 @@ let c1=new Chef("mohamed","ali",20,2000,"informatique");
 console.log(p1.aff());
 console.log(e1.aff());
 console.log(c1.aff());*/
+/*
+import voiture from './voiture.js';
+import affichage from './voiture.js';
+
+let v1=new voiture('188-151','bmw');
+console.log(affichage(v1));
+*/
+//------------------------ template literals-----------------------
+/*
+const nom="mohamed";
+const prenom="ali";
+console.log(`nom+""+prenom`);
+console.log(`${nom} ${prenom}`);
+
+
+
+// les opérateur conditionnel ternaire
+var moy=10;
+var result;
+if (moy>=10){
+    result("admis");
+}else{
+    result("non admis");
+}
+console.log(result);
+var moy=10;
+var result;
+moy>=10 ? result="admis" : result="non admis";
+console.log(result); 
+//exemple:
+let name="mohamed";
+let message=name?"bonjour "+name:"bonjour";
+console.log(message);
+
+------------>*/
+class Document {
+    constructor(numEnreg, titre, auteur) {
+        this.numEnreg = numEnreg;
+        this.titre = titre;
+        this.auteur = auteur;
+    }
+}
+
+class Livre extends Document {
+    constructor(numEnreg, titre, auteur) {
+        super(numEnreg, titre, auteur);
+    }
+    infoLivre() {
+        return `Numéro d'enregistrement: ${this.numEnreg}, Titre: ${this.titre}, Auteur: ${this.auteur}`;
+    }
+}
+
+let livre1 = new Livre(1, "Le Petit Prince", "Antoine de Saint-Exupéry");
+let livre2 = new Livre(2, "Harry Potter à l'école des sorciers", "J.K. Rowling");
+let livre3 = new Livre(3, "1984", "George Orwell");
--- project1/src/voiture.js
+++ project1/src/voiture.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+ /*
+ export default class voiture{
+    constructor(marque,matricule){
+        this.marque=marque;
+        this.matricule=matricule;
+    }
+}
+export default function affichage(vt){
+    return `${vt.marque},${vt.matricule}`;
+}
+*/
+
