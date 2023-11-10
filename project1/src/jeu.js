9d237694d5424648fb17098225ba899fca1988bb
danimohamed <mohameddani993@gmail.com>
2023-11-10 10:54:49 +0100

    last update

--- project1/src/jeu.js
+++ project1/src/jeu.js
@@ -1,22 +1,41 @@
 import React from "react";
-export default class JouDe extends React.Component
-{
-    constructor(){
-        super()
-        this.state={cont:0}
-    }
-    render()
-    {
-        return(
-            <div>
-                <img src="image/icon.jpg"/>
-                <h1>Jeu de</h1>
-                <h2>Face:.....</h2>
-                <h2>Nombre d'essai:{this.state.cont}</h2>
-                <button onClick={()=>this.setState({cont:this.state.cont+1})}>Play</button>
-                <p>bravo vous avez trouver la face cachée</p>
-                <button>Instialiser</button>
-            </div>
-        )
-    }
+
+export default class JeuDe extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cont: 0,
+      resultat: null,
+    };
+  }
+
+  jeu() {
+    const n = Math.floor(Math.random() * 6 + 1);
+    this.setState({ resultat: n, cont: this.state.cont + 1 });
+  }
+
+  initializeGame() {
+    this.setState({ cont: 0, resultat: null });
+  }
+
+  render() {
+    const { resultat } = this.state;
+    const diceImageSrc = resultat ? `image/${resultat}.jpg` : null;
+
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+        <img src="image/icon.jpg" alt="icon" />
+        <h1>Jeu de</h1>
+        {diceImageSrc && <img src={diceImageSrc} alt={`Dice result: ${resultat}`} />}
+        
+        <div>
+          <h2>Face: {resultat}</h2>
+          <h2>Nombre d'essai: {this.state.cont}</h2>
+        </div>
+        <button onClick={() => this.jeu()}>Play</button>
+        {resultat === 3 && <p>Bravo, vous avez trouvé la face cachée, c'est 3!</p>}
+        <button onClick={() => this.initializeGame()}>Initialize</button>
+      </div>
+    );
+  }
 }
