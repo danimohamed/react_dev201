bc5f0065eb776f66582234a53fe31bf5029cc4a2
danimohamed <mohameddani993@gmail.com>
2023-11-25 13:13:22 +0100

    index1.js

--- project1/src/index1.js
+++ project1/src/index1.js
@@ -513,3 +513,32 @@ let  stagiere=[
 stagiere=[...stagiere.filter(s=>s.id==3?s.filiere="dev 202":false)];
 console.log(stagiere);
 ------------>*/
+
+
+export default class Message_State extends React.Component{
+    constructor(){
+        super()
+        this.state={valeur:0}
+    }
+    incrementer(){
+        this.setState({valeur:this.state.valeur+1})
+    }
+    Decrementer(){
+        this.setState({valeur:this.state.valeur-1})
+    }
+    render(){
+        let color = "black";
+        if (this.state.valeur > 20) {
+            color = "green";
+        } else if (this.state.valeur < 0) {
+            color = "red";
+        }
+        return(
+            <div>
+                <h2 style={{color: color}}>Valeur :{this.state.valeur}</h2>
+                <button onClick={()=>this.incrementer()}>Incrementer</button>
+                <button onClick={()=>this.Decrementer()}>Decrementer</button>
+            </div>
+        )
+    }
+}
