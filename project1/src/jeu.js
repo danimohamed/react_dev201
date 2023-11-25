a8416cdbf9558b4f491aa209228911e94ba5986f
danimohamed <mohameddani993@gmail.com>
2023-11-25 13:13:23 +0100

    jeu.js

--- project1/src/jeu.js
+++ project1/src/jeu.js
@@ -1,41 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default class JeuDe extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      cont: 0,
-      resultat: null,
-    };
-  }
+const JeuDe = () => {
+  const [cont, setCont] = useState(0);
+  const [resultat, setResultat] = useState(null);
 
-  jeu() {
+  const jeu = () => {
     const n = Math.floor(Math.random() * 6 + 1);
-    this.setState({ resultat: n, cont: this.state.cont + 1 });
-  }
+    setResultat(n);
+    setCont(cont + 1);
+  };
 
-  initializeGame() {
-    this.setState({ cont: 0, resultat: null });
-  }
+  const initializeGame = () => {
+    setCont(0);
+    setResultat(null);
+  };
 
-  render() {
-    const { resultat } = this.state;
-    const diceImageSrc = resultat ? `image/${resultat}.jpg` : null;
+  const diceImageSrc = resultat ? `image/${resultat}.jpg` : null;
 
-    return (
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-        <img src="image/icon.jpg" alt="icon" />
-        <h1>Jeu de</h1>
-        {diceImageSrc && <img src={diceImageSrc} alt={`Dice result: ${resultat}`} />}
-        
-        <div>
-          <h2>Face: {resultat}</h2>
-          <h2>Nombre d'essai: {this.state.cont}</h2>
-        </div>
-        <button onClick={() => this.jeu()}>Play</button>
-        {resultat === 3 && <p>Bravo, vous avez trouvé la face cachée, c'est 3!</p>}
-        <button onClick={() => this.initializeGame()}>Initialize</button>
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+      <img src="image/icon.jpg" alt="icon" />
+      <h1>Jeu de</h1>
+      {diceImageSrc && <img src={diceImageSrc} alt={`Dice result: ${resultat}`} />}
+      
+      <div>
+        <h2>Face: {resultat}</h2>
+        <h2>Nombre d'essai: {cont}</h2>
       </div>
-    );
-  }
-}
+      
+      {resultat !== 5 && <button onClick={jeu}>Play</button>}
+      
+      {resultat === 5 && <p>Bravo, vous avez trouvé la face cachée, c'est 3!</p>}
+      {resultat === 5 && <button onClick={initializeGame}>Initialize</button>}
+    </div>
+  );
+};
+
+export default JeuDe;
