ce137bdb1b1fbc5df08747237b0590e860cccc39
danimohamed <mohameddani993@gmail.com>
2023-11-25 13:09:55 +0100

    the file index.js uploaded

---
+++ project3/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CalculIMC } from './imc';
+import { Exrv } from './exrv';
+
+
+
+
+
+const root = document.getElementById('root');
+ReactDOM.render(<Exrv />, root);
